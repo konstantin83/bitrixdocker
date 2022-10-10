b3b398bcf08a78d6295edd20b76197416dfc6dd9
Константин Салахутдинов <konstantin.sal@gmail.com>
2022-10-10 23:56:51 +0300

    custom ports for local nginx + openssl setup

--- nginx/ssl.sh
+++ nginx/ssl.sh
@@ -1,6 +1,7 @@
-export SITE_NAME=ms.local
+export SITE_NAME="ms.loc"
 
-openssl req -x509 -out $SITE_NAME.crt -keyout $SITE_NAME.key \
+# openssl req -x509 -out $SITE_NAME.crt -keyout $SITE_NAME.key \
+openssl req -x509 -out public.crt -keyout private.key \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=$SITE_NAME' -extensions EXT -config <( \
    printf "[dn]\nCN=$SITE_NAME\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:$SITE_NAME\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
