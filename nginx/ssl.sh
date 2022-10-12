af53212c686cc59d83d43f08c53071090d19c654
Константин Салахутдинов <konstantin.sal@gmail.com>
2022-10-12 13:05:38 +0300

    new variables

--- nginx/ssl.sh
+++ nginx/ssl.sh
@@ -1,7 +1,10 @@
-export SITE_NAME="ms.loc"
+# export SITE_NAME="pas.loc"
+
+export $(grep -v '^#' ../.env | xargs)
 
 # openssl req -x509 -out $SITE_NAME.crt -keyout $SITE_NAME.key \
-openssl req -x509 -out public.crt -keyout private.key \
+mkdir ssl
+openssl req -x509 -out ssl/public.crt -keyout ssl/private.key \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=$SITE_NAME' -extensions EXT -config <( \
    printf "[dn]\nCN=$SITE_NAME\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:$SITE_NAME\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
